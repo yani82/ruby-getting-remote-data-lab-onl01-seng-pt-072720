4180233f9602dd5f4482a4d4e8eb786f913073f5
Yani Asari <yani.iman1@gmail.com>
2020-09-05 05:02:46 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ class GetRequester
   
   def get_response_body 
     html = File.read('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
-    GetRequester = Nokogiri::HTML(html)
+    GetRequester = NET::HTML(html)
   end 
  
 end 
