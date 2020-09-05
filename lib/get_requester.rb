5bd8826a23f1ef032baed3897b92f7b80bd69e9c
Yani Asari <yani.iman1@gmail.com>
2020-09-05 05:45:04 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -19,37 +19,9 @@ class GetRequester
   def parse_json 
     data = JSON.parse(self.get_response_body)
   end 
-  
-
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
-uri = URI.parse(url)
-response = Net::HTTP.get_response(uri)
 
 
-
-
-  
- 
-    GetRequester = Net::HTTP(http)
-
-    get_response_body = {}
-  
-     parse_json("li.project.grid_4").each do |files|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
-  end
- 
-  # return the projects hash
-  projects
-  
-end
-
-  end 
+end 
   
  
   
