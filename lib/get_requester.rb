fa3cca6456d874e25408827a12868d72acd7d24e
Yani Asari <yani.iman1@gmail.com>
2020-09-05 05:16:09 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -14,20 +14,6 @@ end
 
 get_response_body = {}
   
-    parse_json("li.project.grid_4").each do |files|
-    title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
-      :description => project.css("p.bbcard_blurb").text,
-      :location => project.css("ul.project-meta span.location-name").text,
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-    }
-  end
- 
-  # return the projects hash
-  projects
-end
-
     
   end 
   
