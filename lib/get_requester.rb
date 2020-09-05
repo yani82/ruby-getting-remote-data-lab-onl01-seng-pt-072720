95687ccd2f41e4b88301ef44a8a886ec114afd22
Yani Asari <yani.iman1@gmail.com>
2020-09-05 04:33:35 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -4,7 +4,7 @@ require "pry"
 class GetRequester
   
   def get_response_body 
-    html = File.read('')
+    html = File.read('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
   end 
   def create_project_hash
   html = File.read('fixtures/kickstarter.html')
