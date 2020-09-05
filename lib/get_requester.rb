cc5d2d4046475256f9f8fe3f1d65058959ed3cfc
Yani Asari <yani.iman1@gmail.com>
2020-09-05 05:32:38 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,6 +2,7 @@ require 'open-uri'
 require 'net/http'
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 uri = URI.parse(url)
+response = Net::HTTP.get_response(uri)
 
 class GetRequester
   
